c43c66e3ce15b593f559c3ac135cd0d1e7b50fff
LorTos <lorenzotoscanidc@gmail.com>
2019-01-30 11:59:32 +0100

    Initial setup commit

---
+++ Example/Pods/Target Support Files/LTImagePicker/LTImagePicker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LTImagePicker : NSObject
+@end
+@implementation PodsDummy_LTImagePicker
+@end
---
+++ Example/Pods/Target Support Files/Pods-LTImagePicker_Tests/Pods-LTImagePicker_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LTImagePicker_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LTImagePicker_Tests
+@end
